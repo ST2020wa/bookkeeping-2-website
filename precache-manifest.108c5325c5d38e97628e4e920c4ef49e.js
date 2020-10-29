f56f90b00fd786c9b501e3e553ad46e6ac8ba2bf
ST <shaotian.li.april@gmail.com>
2020-10-29 12:14:47 +0800

    init

---
+++ precache-manifest.108c5325c5d38e97628e4e920c4ef49e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba5ed51d695afefcaa578a567cd7de0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b437b461a794312a96bf",
+    "url": "/static/css/main.dbccb7cb.chunk.css"
+  },
+  {
+    "revision": "190cb5d9f166c96a96d0",
+    "url": "/static/js/2.def18ee4.chunk.js"
+  },
+  {
+    "revision": "e4e6e48a9ecea75dc1966a170f826888",
+    "url": "/static/js/2.def18ee4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b437b461a794312a96bf",
+    "url": "/static/js/main.659de1ce.chunk.js"
+  },
+  {
+    "revision": "82577cc2006812eeac28",
+    "url": "/static/js/runtime-main.8aedb875.js"
+  }
+]);
