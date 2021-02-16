574a5357013ae9e9b1fd65f4a1750ef24e5aa66e
ST Li <kidtime@126.com>
2021-02-16 14:31:56 +0800

    deploy

---
+++ precache-manifest.c1ec53d0c809149a939000e01dc972ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca41d9d991db61deadd70d795075771e",
+    "url": "/bookkeeping-2-website/index.html"
+  },
+  {
+    "revision": "3478656c0d38f5f967b7",
+    "url": "/bookkeeping-2-website/static/css/main.9ac8f82d.chunk.css"
+  },
+  {
+    "revision": "2d99677a8c8090ad582b",
+    "url": "/bookkeeping-2-website/static/js/2.8a0d3f26.chunk.js"
+  },
+  {
+    "revision": "e4e6e48a9ecea75dc1966a170f826888",
+    "url": "/bookkeeping-2-website/static/js/2.8a0d3f26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3478656c0d38f5f967b7",
+    "url": "/bookkeeping-2-website/static/js/main.06300837.chunk.js"
+  },
+  {
+    "revision": "bd4257ace638523f1532",
+    "url": "/bookkeeping-2-website/static/js/runtime-main.c4cd05fa.js"
+  }
+]);
